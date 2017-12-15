8d06cf2b1cdc48cfe9d45de9574edcedd5e5bc17
Selvaggia-S <33578819+Selvaggia-S@users.noreply.github.com>
2017-12-15 18:07:10 +0100

    Assets - scenes + scripts

---
+++ Museum/Assets/Scripts/VideosStart.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideosStart : MonoBehaviour
+{
+    public VideoPlayer Video_shoot, Video_bomb1, Video_bomb2, Video_host, Video_med;
+
+    public void PlayShoot()
+    {
+        // Video_shoot.Play ();
+
+        if (Video_shoot.isPlaying)
+        {
+            Video_shoot.Pause();
+
+        }
+        else
+        {
+            Video_shoot.Play();
+        }
+
+
+
+    }
+
+    public void PlayBomb1()
+    {
+        //Video_bomb1.Play();
+        if (Video_bomb1.isPlaying)
+        {
+            Video_bomb1.Pause();
+
+        }
+        else
+        {
+            Video_bomb1.Play();
+        }
+
+    }
+
+    public void PlayBomb2()
+    {
+        //Video_bomb2.Play();
+        if (Video_bomb2.isPlaying)
+        {
+            Video_bomb2.Pause();
+
+        }
+        else
+        {
+            Video_bomb2.Play();
+        }
+
+    }
+
+    public void PlayHost()
+    {
+        //Video_host.Play();
+        if (Video_host.isPlaying)
+        {
+            Video_host.Pause();
+
+        }
+        else
+        {
+            Video_host.Play();
+        }
+    }
+
+    public void PlayMed()
+    {
+        //Video_med.Play();
+
+        if (Video_med.isPlaying)
+        {
+            Video_med.Pause();
+
+        }
+        else
+        {
+            Video_med.Play();
+        }
+    }
+}
+
+
---
+++ Museum/Assets/Scripts/start_exit.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_exit : MonoBehaviour
+{
+    public GameObject startUI, restartUI, instructionUI;
+    public GameObject closebutton_HostageUI, closebutton_MedUI, closebutton_ShootUI, closebutton_BombUI;
+
+    // Use this for initialization
+    void Start() { }
+
+    public void closebuttonHostage()
+        //Use to close a panel through buttons
+    {
+        closebutton_HostageUI.SetActive(false);
+    }
+    public void closebuttonMed()
+    //Use to close a panel through buttons
+    {
+        closebutton_MedUI.SetActive(false);
+    }
+    public void closebuttonShoot()
+    //Use to close a panel through buttons
+    {
+        closebutton_ShootUI.SetActive(false);
+    }
+
+    public void closebuttonBomb()
+    //Use to close a panel through buttons
+    {
+        closebutton_BombUI.SetActive(false);
+    }
+
+    // Begin the sequence.
+    public void StartMuseum()
+    {  // Enable the start UI.
+        startUI.SetActive(false);
+        instructionUI.SetActive(true);
+    }
+
+    public void BeginTour()
+    {
+        instructionUI.SetActive(false);
+
+    }    
+    public void exitbutton ()
+    { 
+       // Disable the start UI.
+       startUI.SetActive(false);
+
+
+    }
+
+    
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        {
+            Debug.Log("Saliendo");
+            Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        }
+        
+    }
+}
+
